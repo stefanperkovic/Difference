40a37e5c26d3a6e4db6d9063bd9b96708057b55e
stefanperkovic <stefan.perkovic@menloschool.org>
2025-02-26 10:27:34 -0800

    Got close to finishing the ps

--- src/PlagiarismChecker.java
+++ src/PlagiarismChecker.java
@@ -15,12 +15,25 @@ public class PlagiarismChecker {
      * @return The length of the longest shared substring.
      */
     public static int longestSharedSubstring(String doc1, String doc2) {
-
         // TODO Complete this function to return the length of the longest shared substring.
 
+        int[][] arr = new int[doc1.length() + 1][doc2.length() + 1];
 
-
-
-        return 0;
+        for (int i = 1; i <= doc1.length(); i++){
+            for (int j = 1; j <= doc2.length(); j++){
+                if (doc1.charAt(i) == doc2.charAt(j)) {
+                    arr[i][j] = arr[i - 1][j - 1] + 1;
+                }
+                else{
+                    if (arr[i][j - 1] > arr[i - 1][j]){
+                        arr[i][j] = arr[i][j - 1];
+                    }
+                    else{
+                        arr[i][j] = arr[i- 1][j];
+                    }
+                }
+            }
+        }
+        return arr[doc1.length()][doc2.length()];
     }
 }
