dc381d2f6f5cb313ba51d9c6219a645fdf0b1866
stefanperkovic <stefan.perkovic@menloschool.org>
2025-03-03 12:59:12 -0800

    Added Comments

--- src/PlagiarismChecker.java
+++ src/PlagiarismChecker.java
@@ -15,14 +15,18 @@ public class PlagiarismChecker {
      * @return The length of the longest shared substring.
      */
     public static int longestSharedSubstring(String doc1, String doc2) {
-
+        // Creates 2D array to store the longest subsequences at given indexes
         int[][] arr = new int[doc1.length() + 1][doc2.length() + 1];
 
+        // Nested for loop to go over every character pair in the array
         for (int i = 1; i <= doc1.length(); i++){
             for (int j = 1; j <= doc2.length(); j++){
+                // Checks if the two characters are equal
                 if (doc1.charAt(i - 1) == doc2.charAt(j - 1)) {
+                    // Goes diagonally before and adds one to that value
                     arr[i][j] = arr[i - 1][j - 1] + 1;
                 }
+                // Take the maximum value from the top or left cell
                 else{
                     if (arr[i][j - 1] > arr[i - 1][j]){
                         arr[i][j] = arr[i][j - 1];
@@ -33,6 +37,7 @@ public class PlagiarismChecker {
                 }
             }
         }
+        // Returns the length of the longest common subsequence
         return arr[doc1.length()][doc2.length()];
     }
 }
